fa6e475a320db77c5dde703a8f6a1bb6792f764b
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-05-14 11:18:35 +0600

    First commit for creating larave assignment project

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/assignment', function () {
+    return "Hello World";
+});
+
+
+
+
+
+
